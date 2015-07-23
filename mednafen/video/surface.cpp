912c09527b6c2eb76c8f6c2dc7be75b604702ca6
twinaphex <libretro@gmail.com>
2015-07-23 16:08:25 +0200

    (surface.cpp) Cleanups

--- mednafen/video/surface.cpp
+++ mednafen/video/surface.cpp
@@ -20,8 +20,6 @@
 
 MDFN_Surface::MDFN_Surface()
 {
-   memset(&format, 0, sizeof(format));
-
    pixels16 = NULL;
    pitchinpix = 0;
    w = 0;
@@ -36,8 +34,6 @@ MDFN_Surface::MDFN_Surface(void *const p_pixels, const uint32 p_width, const uin
 void MDFN_Surface::Init(void *const p_pixels, const uint32 p_width, const uint32 p_height, const uint32 p_pitchinpix, const MDFN_PixelFormat &nf)
 {
    void *rpix = NULL;
-
-   format = nf;
 
    pixels16 = NULL;
 
@@ -57,7 +53,6 @@ void MDFN_Surface::Init(void *const p_pixels, const uint32 p_width, const uint32
 // to boot.
 void MDFN_Surface::SetFormat(const MDFN_PixelFormat &nf, bool convert)
 {
-   format = nf;
 }
 
 MDFN_Surface::~MDFN_Surface()
