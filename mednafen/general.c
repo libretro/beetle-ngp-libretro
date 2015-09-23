9042a905c0fd3e0dbec453972e0b6c23f54a4636
twinaphex <libretro@gmail.com>
2015-09-23 17:15:23 +0200

    Convert general.cpp to C

--- mednafen/general.c
+++ mednafen/general.c
@@ -15,16 +15,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "mednafen.h"
-
-#include <string.h>
 #include <stdarg.h>
+#include <string.h>
 
 #include <sys/types.h>
-
-#include <string>
-
-#include "general.h"
 
 const char * GetFNComponent(const char *str)
 {
