6dc6d4fdd4aebe65a6b1316d8dd75f87219e1389
libretroadmin <reallibretroretroarch@gmail.com>
2022-06-17 22:50:54 +0200

    Update to version 0.9.35.5 - no functional changes

--- libretro.c
+++ libretro.c
@@ -278,7 +278,7 @@ static bool update_audio = false;
 
 #define MEDNAFEN_CORE_NAME_MODULE "ngp"
 #define MEDNAFEN_CORE_NAME "Beetle NeoPop"
-#define MEDNAFEN_CORE_VERSION "v0.9.36.3"
+#define MEDNAFEN_CORE_VERSION "v0.9.36.5"
 #define MEDNAFEN_CORE_EXTENSIONS "ngp|ngc|ngpc|npc"
 #define MEDNAFEN_CORE_TIMING_FPS 60.25
 #define MEDNAFEN_CORE_GEOMETRY_BASE_W 160 
